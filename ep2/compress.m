2c664495c95990b921b94538597bc307f46882b1
dsnunes07 <dsnunes@usp.br>
2020-06-16 11:19:16 -0300

    remove print n rows

--- ep2/compress.m
+++ ep2/compress.m
@@ -1,6 +1,6 @@
 function compress (originalImg, k)
     img = imread(originalImg);
-    n = rows(img)
+    n = rows(img);
     resultImg = img(1:k+1:n, 1:k+1:n, :);
     imwrite(resultImg, "compressed.png");
 endfunction
